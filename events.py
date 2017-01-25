df301a66848bb38057f31ca55893336250cf1406
Wanjiru Ndung'u <daisy@localhost.localdomain>
2017-01-25 12:20:55 +0300

    Updated Function generate tickets

--- events.py
+++ events.py
@@ -46,11 +46,11 @@ def add_items():
 
 
 def view_all():
-        # print ("All Events (Open and Closed)")
-    conn.execute("SELECT * FROM event")
-    items = conn.fetchall()
-    for row in items:
-        print(row)
+    try:      # print ("All Events (Open and Closed)")
+        conn.execute("SELECT * FROM tickets")
+        items = conn.fetchall()
+        for row in items:
+            print(row)
         # print ("Event ID = ", row[0])
         # print ("EVENT NAME = ", row[1])
         # print ("START DATE = ", row[2])
@@ -58,7 +58,9 @@ def view_all():
         # print ("DESCRIPTION = ", row[4])
         # print ("NUMBER OF TICKETS = ", row[5])
         # print ("STATUS = ", row[6]), "\n"
-
+        print ("***____________All Events Displayed Above___________***")
+    except:
+        print ("Error in printing")
 # Function to delete Events
 
 
@@ -80,7 +82,7 @@ def edit_event():
         "Use name to edit event name, start_date to edit when the event begin, end_date to edit when the event ends and venue to edit the venue")
     print("Example: What would you like to edit?: venue ")
     label = str(input("What would you like to edit?:  "))
-    # split string into list
+    # compares user input to the set conditions
     if label == 'name':
         event_id = int(input("Enter te ID of the event to be modified: "))
         if type(event_id) is not int:
@@ -145,20 +147,29 @@ def edit_event():
 
 def generate_ticket():
     conn.execute("CREATE TABLE IF NOT EXISTS tickets( \
-                    ticket_id INTEGER PRIMARY KEY, owner_fname TEXT, owner_lname TEXT, owner_email TEXT, event_id INTEGER, event_name TEXT, start_date DATE, end_date DATE, venue TEXT ) ")
+                    ticket_id INTEGER PRIMARY KEY, event_id INTEGER, owner_fname TEXT, owner_lname TEXT, owner_email TEXT, event_name TEXT, start_date DATE, end_date DATE, venue TEXT, FOREIGN KEY (event_id) REFERENCES event(event_id)) ")
 
     print ("Generate Tickets")
     print ("")
 
     event_id = input ("Enter Event ID: ")
     owner_fname = input ("Enter Your First Name: ")
-    owner_fname = input ("Enter Your Last Name: ")
+    owner_lname = input ("Enter Your Last Name: ")
     owner_email = input ("Enter your Email: ")
     try:
         # run sql command and commit data to db
-        conn.execute("INSERT INTO tickets VALUES (null,?,?,?,event_id,event_name, start_date, end_date, venue) ;",
-                     (event_name, start, end, venue))
-        db_con.commit()
+        item = conn.execute("SELECT event_name, start_date, end_date, venue FROM event WHERE event_id=?",(event_id))
+        for column in item:
+            name = column [0]
+            s_date = column [1]
+            e_date = column [2]
+            venue = column [3]
+            # print ("name: " + name) conn.execute("INSERT INTO event VALUES (null,?,?,?,?);",
+                     # (event_name, start, end, venue))
+            # print ("Start Date: " + s_date)
+            conn.execute("INSERT INTO tickets VALUES (null,?,?,?,?,?,?,?,?);",
+                            (owner_fname, owner_lname, owner_email, name, s_date, e_date, venue, event_id))
+            db_con.commit()
         print("***Data saved data...........***")
     except:
         print("***Error in saving data...........***")
@@ -166,4 +177,4 @@ def generate_ticket():
 
 if __name__ == '__main__':
 
-    edit_event()
+    view_all()
