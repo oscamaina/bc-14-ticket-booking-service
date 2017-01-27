6a1c6d30922d10498468341d9c45bed60b5e6d40
Daisy <daisywndungu@gmail.com>
2017-01-27 11:10:33 +0300

    Add comments in codes

--- interface.py
+++ interface.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 """
+This example uses docopt with the built in cmd module to demonstrate an
+interactive command application.
 
 Usage:
     events 411 add_events <event_name>
     events 411 view_all <table_name>
     events 411 delete_event <eventid>
     events 411 edit_event <event_id>
+    events 411 generate_ticket <event_id>
     events 411 ticket_invalidation <ticket_id> 
-
 
 Options:
     -i, --interactive  Interactive Mode
@@ -72,7 +74,7 @@ class MyInteractive (cmd.Cmd):
 
     @docopt_cmd
     def do_add_event(self, arg):
-        """Usage: add_events <event_name> """
+        """Usage: add_event <event_name> """
 
         event_name = arg['<event_name>']
         events.add_event(event_name)
@@ -96,14 +98,21 @@ class MyInteractive (cmd.Cmd):
         """Usage: edit_event <event_id> """
 
         event_id = arg['<event_id>']
-        events.edit_event(event_id)
+        events.edit_events(event_id)
 
     @docopt_cmd
     def do_generate_ticket(self, arg):
-        """Usage: edit_event <event_id> """
+        """Usage: generate_ticket <event_id> """
 
         event_id = arg['<event_id>']
         events.generate_ticket(event_id)
+
+    @docopt_cmd
+    def do_ticket_validation(self, arg):
+        """Usage: ticket_validation <ticket_id> """
+
+        ticket_id = arg['<ticket_id>']
+        events.ticket_validation(ticket_id)
 
     @docopt_cmd
     def do_ticket_invalidation(self, arg):
